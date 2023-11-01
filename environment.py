8c0cf5e0d408ade334a74ef0c63fa89bb40273ff
AmirHShabani <97363432+AmirHShabani@users.noreply.github.com>
2023-11-01 06:40:44 +0330

    Add step() function

--- environment.py
+++ environment.py
@@ -25,6 +25,7 @@ class ShoverWorldEnv(gym.Env):
         self.map = [[0 for j in range(self.n_cols)] for i in range(self.n_rows)]
         self.last_pos = None
         self.last_dir = None
+        self.time_step = 1 
         self._generate_map()
         
     def reset(self):
@@ -33,8 +34,73 @@ class ShoverWorldEnv(gym.Env):
         self.last_pos = None
         self._generate_map()
 
-    def step(self, action):
-        pass
+
+    def step(self, action): # action : tuple(row, column, direction)
+        
+        self.time_step += 1
+        if self.time_step > self.max_timestep:
+            return self._get_obs(), 0, True, self._get_info()
+
+        reward = 0 
+        # Check if the action is the same as the last one
+        if self.last_pos == (action[0], action[1]) and self.last_dir==action[2]:
+            reward += 1 
+        
+        # update the last pose and dir 
+        self.last_pos = (action[0], action[1])
+        self.last_dir = action[2]
+             
+        directions = {
+            0 : (-1, 0), # Up 
+            1 : (0, 1),  # Right 
+            2 : (1, 0),  # Down 
+            3 : (0, -1)  # Left
+        }
+
+        self.last_action = action
+        row, col = action[0], action[1]
+        dr, dc = directions[action[2]]
+
+        if self.map[action[0]][action[1]] != 1: # if the selected point is a box
+            return self.map 
+        else: 
+            # Counting boxes in the same direction line
+            count_one = 0 
+            while self.map[row][col] == 1: 
+                count_one += 1 
+                row += dr 
+                col += dc 
+            row -= dr 
+            col -= dc 
+            reward -= count_one 
+        
+
+
+        # Moving the box and any boxes in line with it (recursively) 
+        for _ in range(count_one): 
+            if (0 <= row+dr < len(self.map) and 0 <= col+dc < len(self.map[0]) and self.map[row+dr][col+dc] != 2):
+                # If next point is lava, remove the box
+                if self.map[row+dr][col+dc] == -1: 
+                    self.map[row][col] = 0 
+                    row -= dr 
+                    col -= dc               
+                # If next point is empty, move the box
+                elif self.map[row+dr][col+dc] == 0:
+                    self.map[row][col], self.map[row+dr][col+dc] = self.map[row+dr][col+dc], self.map[row][col]
+                    print(self.map[row][col], self.map[row+dr][col+dc])
+                    row -= dr 
+                    col -= dc 
+                else:
+                    break 
+
+            else:
+                break 
+        
+        if not any(1 in row for row in self.map):
+            return self._get_obs(), reward, True, self.get_info()
+        else:
+            return self._get_obs(), reward, False, self._get_info()
+        
 
     def _generate_map(self):
         '''
